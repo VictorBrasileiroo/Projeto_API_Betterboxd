fd3883e13ef01f68789535d8a038b0f15e5b61cf
Your Name <victor.lopesvfk11@gmail.com>
2025-04-11 17:15:36 -0300

    feat(core): criação do repository de avalicao

---
+++ Betterboxd.Core/Interfaces/IAvaliacaoRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Betterboxd.Core.Entities;
+
+namespace Betterboxd.Core.Interfaces
+{
+    public interface IAvaliacaoRepository
+    {
+        Task<List<AvaliacaoModel>> GetAll();
+        Task<AvaliacaoModel> GetById(int id);
+        Task<List<AvaliacaoModel>> GetAllByUserId(int idUser);
+        Task<List<AvaliacaoModel>> GetAllByFilmId(int idFilm);
+        Task<AvaliacaoModel> CalcularNotaMediaFilme(int idFilm);
+        Task<AvaliacaoModel> Create(AvaliacaoModel user);
+        Task<AvaliacaoModel> Update(AvaliacaoModel user);
+        Task<AvaliacaoModel> Delete(AvaliacaoModel user);
+    }
+}
